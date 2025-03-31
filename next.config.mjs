ba2e14264e60c7878887a3a53b7df53d39ae8cfd
alic3 <alic3dev@gmail.com>
2025-03-30 21:48:06 -0700

    dependencies:add->

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: function next_config_webpack_config(config) {
+    config.module.rules.push({
+      test: /.node$/,
+      loader: 'node-loader'
+    });
+    
+    return config
+  }
+};
 
 export default nextConfig;
